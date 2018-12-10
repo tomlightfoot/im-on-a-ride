7a67c349b52b03242438c4185b2ab4ed0b68f9d0
sacullezzar <lucas.razzell@holidayextras.com>
2018-12-10 16:26:03 +0000

    basic build of front page, with buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Button from './components/button'
 import './App.css';
+
+const title = <h1>I'm on a Ride!</h1>
 
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
-    );
+      <div id='king'>
+      <div id='title'>
+        {title}
+      </div>
+      <div>
+        <Button name='Thorpe Park'/>
+        <Button name='Alton Towers'/>
+        <Button name='Chessington: World of Adventures'/>
+        <Button name='LegoLand'/>
+      </div>
+      </div>
+    )
   }
 }
 
